13204737a24bb36d001dfee67c080eaf6b58055f
denis-fominykh <fominykhdenis@gmail.com>
2020-03-19 20:51:35 +0200

    creating a directory for the store

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import './App.scss';
-import reducer from './reducers/reducer';
+import store from './store/store';
 import Counter from './components/Counter';
-
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
 
 const App = () => {
   return (
