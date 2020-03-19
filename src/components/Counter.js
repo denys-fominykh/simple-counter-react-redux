8df748d069b4be4f30fc0d5e281b31e1f01d52cb
denis-fominykh <fominykhdenis@gmail.com>
2020-03-19 20:45:03 +0200

    connecting the redux with the react

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from '../actions/actions';
 
-const Counter = () => {
+const Counter = ({ counter, inc, dec, rnd }) => {
   return (
     <div className="jumbotron">
-      <h2 className="counter">0</h2>
-      <button className="btn btn-success btn-lg mr-1 inc">INC</button>
-      <button className="btn btn-danger btn-lg mr-1 dec">DEC</button>
-      <button className="btn btn-dark btn-lg rnd">RND</button>
+      <h2 className="counter">{counter}</h2>
+      <button className="btn btn-success btn-lg mr-1 inc" onClick={inc}>INC</button>
+      <button className="btn btn-danger btn-lg mr-1 dec" onClick={dec}>DEC</button>
+      <button className="btn btn-dark btn-lg rnd" onClick={rnd}>RND</button>
     </div>
   )
 }
 
-export default Counter;
+const mapStateToProps = state => {
+  return {
+    counter: state,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  const { dec, inc, rnd } = bindActionCreators(actions, dispatch);
+
+  return {
+    dec,
+    inc,
+    rnd: () => {
+      const randomValue = Math.floor(Math.random()*10);
+      rnd(randomValue);
+    },
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
